2f1ccc2edbf618f56bdec5dae38bc7c6c68ece86
pgabriella <81381710+pgabriella@users.noreply.github.com>
2021-07-29 17:02:34 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Radno vrijeme:<br> 8-22";
+  }
+
+function myLocation(){
+    document.getElementById("demo1").innerHTML = "Istarska Ulica 3<br>21000 Split";
+    
+
+}
+
+function myContact(){
+    document.getElementById("demo2").innerHTML ="fitnessclub@gmail.com"
+}
+
+document.getElementById("demo").style.color ='#FFFFFF';
+document.getElementById("demo1").style.color='#FFFFFF';
+document.getElementById("demo2").style.color='#FFFFFF';
+
